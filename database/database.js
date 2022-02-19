a7900816d711ee8792dab4b6522a0ef5ec05c226
vhisla <eahisla@gmail.com>
2022-02-19 16:01:12 -0300

    Add database connection

--- database/database.js
+++ database/database.js
@@ -1,3 +1,8 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('')
+const connection = new Sequelize('guiaperguntas','root','',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+module.exports = connection
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const connection = require('./database/database')
+const Pergunta = require('./database/Pergunta')
+const res = require('express/lib/response')
+
+connection.authenticate().then(()=>{
+    console.log('funcionou o bd')
+}).catch(()=>{
+    console.log('não funcionou o bd :(')
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -14,12 +23,31 @@ app.get('/', (req,res) => {
 
 })
 
+app.get('/perguntas', (req,res)=>{
+
+    Pergunta.findAll({raw: true}).then((perguntas)=>{
+
+        res.render('perguntas', {
+            perguntas: perguntas
+        })
+
+    })
+
+})
+
 app.post('/salvarpergunta', (req,res) => {
 
     let titulo = req.body.titulo
     let descricao = req.body.descricao
 
-    res.send('pergunta enviada com sucesso!!!'+titulo+' - '+descricao)
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=>{
+        res.redirect('/')
+    }).catch(()=>{
+        res.send('ocorreu algum erro, desculpa!')
+    })
 
 })
 
